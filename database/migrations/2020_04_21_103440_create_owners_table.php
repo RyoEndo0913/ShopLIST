efca86106857a456590179b9ee1f54063fa1c79c
Ryo <4889.re@gmail.com>
2020-04-21 22:58:21 +0900

    出店リポジトリ作成

---
+++ database/migrations/2020_04_21_103440_create_owners_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOwnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('owners', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('owner_name_kanji');
+            $table->string('owner_name_kana');
+            $table->date('owner_birth');
+            $table->integer('owner_age');
+            $table->string('owner_address');
+            $table->integer('owner_sex');
+            $table->string('owner_tell');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('owners');
+    }
+}
---
+++ resources/views/owners/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<head>
+    <title>出店表</title>
+</head>
+<body>
+    <h1>出店者一覧</h1>
+    <div>
+        {{$owners->}}
+    </div>
+</body>
