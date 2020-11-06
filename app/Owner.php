1771c2c2d84a438642acb1753a29c00fe5b5797b
Ryo <4889.re@gmail.com>
2020-11-06 22:51:17 +0900

    update:一覧ページのメインデザイン調整
    電話番号タップで発信など

--- app/Owner.php
+++ app/Owner.php
@@ -9,13 +9,18 @@ class Owner extends Model
     protected $table = 'owners';
 
 
+    // 生年月日フォーマット
+    public function getBirthdayAttribute() {
+        return date('Y年m月d日', strtotime($this->owner_birth));
+    }
+
     public function getUnitAgeAttribute() {
         return $this->owner_age .'歳';
     
     }
 
+    // 性別フラグを変換して返す
     public function getOwnerSexAttribute() {
-
         if($this->attributes['owner_sex'] == 1) {
             return "男";
         } else {
--- resources/views/owners/index.blade.php
+++ resources/views/owners/index.blade.php
@@ -1,34 +1,36 @@
 <!DOCTYPE html>
-<meta charset="utf-8">
-<head>
+
+<head>    
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    
     <title>出店表</title>
 </head>
 <body>
     <h1>出店者一覧</h1>
-    <div>
-        <button>登録</button>
-        <button>削除</button>
-        <br>
-            <div>
+    <button class="btn">登録</button>
+        <div class="mt-10">
+            {{-- <input type="checkbox"> --}}
+            <table>
+                <tr>
+                    <th>名前（ふりがな）</th>
+                    <th>生年月日（年齢）</th>
+                    <th>性別</th>
+                    <th>電話番号</th>
+                </tr>
                 @foreach($owners as $owner)
-                    {{-- <input type="checkbox"> --}}
-                    <p>名前(ふりがな)
-                        {{$owner->owner_name_kanji}}
-                        {{$owner->owner_name_kana}}
-                    </p>
-                    <p>生年月日
-                        {{$owner->owner_birth}}
+                <tr>
+                    <td>{{$owner->owner_name_kanji}}
+                        （{{$owner->owner_name_kana}}）
+                    </td>
+                    <td>{{$owner->birth_day}}
                         {{$owner->unit_age}}
-                    </p>
-                    <p>性別
-                        {{$owner->owner_sex}}
-                    </p>
-                    <p>住所
-                        {{$owner->owner_address}}
-                    </p>
-                    <p>電話番号
-                        {{$owner->owner_tell}}<br>
-                    </p>
+                    </td>
+                    <td>{{$owner->owner_sex}}</td>
+                    <td><a href="tel:{{$owner->owner_tell}}">{{$owner->owner_tell}}</a></td>
+                </tr>
                 @endforeach
-            </div>
+            </table>
+        </div>
 </body>
